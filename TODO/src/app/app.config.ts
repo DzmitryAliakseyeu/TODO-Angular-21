2afd0225667d3528fa1880183e8aaa8df18e7f83
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-08-17 14:25:13 +0200

    feat: create form template

--- TODO/src/app/app.config.ts
+++ TODO/src/app/app.config.ts
@@ -1,5 +1,7 @@
-import { ApplicationConfig} from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [provideZonelessChangeDetection(), provideRouter(routes)]
 }
---
+++ TODO/src/app/pages/layout/layout/layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Form } from '../../../shared/form/form/form';
+@Component({
+  selector: 'app-layout',
+  imports: [Form],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss',
+})
+export class Layout {
+
+}
